cf48f8c591c1dedfd07f393e1ab938f779f4fa3d
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-09-30 20:45:57 +0300

    В таблицу довлен столбик submitted_at

--- check_table.sql
+++ check_table.sql
@@ -13,6 +13,7 @@ create table if not exists "applications" (
   "about" varchar(4096) not null,
   "join_reason" varchar(1024) not null,
   "inviter" varchar(256) null,
+  "submitted_at" TIMESTAMPTZ not null default NOW(),
   "ai_decision" application_decision_enum not null default 'Pending',
   "admin_decision" application_decision_enum not null default 'Pending',
   "ai_answer" VARCHAR(1024),
@@ -25,6 +26,7 @@ comment on column "applications"."age" is 'Возраст игрока 1-80 ле
 comment on column "applications"."about" is 'Поле "О себе" длинной не более 4096 символов';
 comment on column "applications"."join_reason" is 'Поле "Почему хотите вступить" длинной не более 1024 символов';
 comment on column "applications"."inviter" is 'Поле "Кто вас приласил" длинной не более 256 символов, опционально и может быть null';
+comment on column "applications"."submitted_at" is 'Дата и время когда была создана заявка';
 comment on column "applications"."ai_decision" is 'Мнение ИИ, принят или нет';
 comment on column "applications"."admin_decision" is 'Мнение админов принят или нет';
 comment on column "applications"."ai_answer" is 'Ответ ИИ самому игроку';
